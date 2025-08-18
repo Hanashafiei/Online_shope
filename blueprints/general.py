01de4f7000679694d3e93e5bec4868daefd801a0
Hana shafiei <tara.shafiei.15@gmail.com>
2025-08-18 20:04:25 +0330

    route (about) added

--- blueprints/general.py
+++ blueprints/general.py
@@ -5,5 +5,10 @@ from flask import Blueprint
 general= Blueprint("general",__name__)
 
 @general.route("/")
-def Home():
+def home():
     return "this is main"
+
+
+@general.route("/about")
+def about():
+    return "about us"
