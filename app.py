bb92bab5da5a1cfb166272a149b02e8a66f2e191
Hana shafiei <tara.shafiei.15@gmail.com>
2025-08-20 14:24:21 +0330

    config added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from blueprints.general import general
 from blueprints.admin import admin
 from blueprints.user import user
@@ -9,6 +10,12 @@ app.register_blueprint(general)
 app.register_blueprint(admin)
 app.register_blueprint(user)
 
+db=SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:@localhost/database"
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
