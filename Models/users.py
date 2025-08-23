e4c49d690637f8a1f5c46b7dd0b6bfb479c2055f
Hana shafiei <tara.shafiei.15@gmail.com>
2025-08-23 21:06:37 +0330

    update user , users

--- Models/users.py
+++ Models/users.py
@@ -1,6 +1,11 @@
 from sqlalchemy import*
+from extentions import *
 
 class User(db.Model):
-    __tablename__="users"
-    id=column()
+    __tablename__ = "users"
     
+    id = Column(Integer, primary_key=True)
+    username = Column(String(100),unique=True, nullable=False,index=True)
+    password = Column(String(50), nullable=False,index=True)
+    phone = Column(String(11),nullable=False,index=True)
+    addres= Column(String(200),nullable=False,index=True)
