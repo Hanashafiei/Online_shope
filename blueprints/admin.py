723ba6c972057a579f447d0e6e76d093aa321bb6
Hana shafiei <tara.shafiei.15@gmail.com>
2025-08-23 22:27:24 +0330

    update admin

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,10 +1,21 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
+from config import *
 
 
 
 admin= Blueprint("admin",__name__)
 
-@admin.route("/admin/login")
+@admin.route("/admin/login",methods=["POST" , "GET"])
 def login():
-    return render_template("admin/login.html")
+    if request.method == "post":
+        username=request.form.get('username',None)
+        password=request.form.get('password',None)
+
+        if username ==ADMIN_USERNAME and password==PASSWORD_USERNAME:
+            pass
+
+
+
+    else:
+        return render_template("admin/login.html")
 
--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:@localhost/online_shop"
 SECRET_KEY="rw,jgho;eyiuh6opuje[ht]gu668h57v69e/tbn fy2erygu43yt5[55ljfbghkrhbklvj"
+
+ADMIN_USERNAME="admin"
+PASSWORD_USERNAME="1234"
