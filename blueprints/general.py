9977f1144d42d9dcf9d1f01dc27e4f5771ba03e2
Hana shafiei <tara.shafiei.15@gmail.com>
2025-08-26 22:02:57 +0330

    create about , home

--- blueprints/general.py
+++ blueprints/general.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 
 
@@ -6,9 +6,9 @@ general= Blueprint("general",__name__)
 
 @general.route("/")
 def home():
-    return "this is main"
+    return render_template("home.html")
 
 
 @general.route("/about")
 def about():
-    return "about us"
+    return render_template("about.html")
