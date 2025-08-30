8a6579445db3c763f2f2c243b90f603aefced6cf
Hana shafiei <tara.shafiei.15@gmail.com>
2025-08-30 17:31:06 +0330

    Model cart added

---
+++ Models/cart.py
@@ -0,0 +1,12 @@
+from sqlalchemy import*
+from extentions import *
+from flask_login import UserMixin
+
+class Cart(db.Model,UserMixin):
+    __tablename__ = "carts"
+    
+    id = Column(Integer, primary_key=True)
+    status=Column(String(200),default="pending")
+    user_id = Column(Integer,ForeignKey('users.id'), nullable=False,index=True)
+    user=db.relationship('User',back_populates='carts')
+    
