944cb049d20ab16f9e0d6cf4fa58e03beccf54be
Hana shafiei <tara.shafiei.15@gmail.com>
2025-09-02 22:41:17 +0330

    remove_from_cart added

--- blueprints/user.py
+++ blueprints/user.py
@@ -84,6 +84,24 @@ def add_to_cart():
 
        return redirect(url_for('user.cart'))
 
+
+
+                            
+@user.route("/remove-from-cart", methods=["GET"])
+@login_required
+def remove_from_cart(): 
+       id=request.args.get("id")  
+       cart_item=CartItem.query.filter(CartItem.id==id).first_or_404()
+       if cart_item.quantity >1:
+              cart_item.quantity -=1
+
+       else:
+              db.session.delete(cart_item)
+              
+       db.session.commit()
+
+       return redirect(url_for('user.cart'))
+
 @user.route("/cart", methods=["GET"])
 @login_required
 def cart(): 
